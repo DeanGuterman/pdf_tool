27a2d33d26b0c79052f670c3b8c5b552f9ba7190
DeanGuterman <deangut@gmail.com>
2024-06-10 01:17:38 +0300

    Add documentation

--- src/merger.py
+++ src/merger.py
@@ -3,16 +3,26 @@ from PyPDF2 import PdfMerger
 
 
 def merge_pdfs(input_folder, output_folder):
+    """
+    Merge multiple pdfs into a new singular pdf
+    :param input_folder: The folder containing the pdfs to be merged
+    :param output_folder: The folder that should hold the newly created pdf
+    """
+
+    # Clear the output folder
+    # TODO: CLEAN CODE THE REPEATS IN SPLITTER.PY
     for file in os.listdir(output_folder):
         file_path = os.path.join(output_folder, file)
         if os.path.isfile(file_path):
             os.remove(file_path)
 
+    # Iterate through the pdfs in the input folder, while merging them into a merger
     merger = PdfMerger()
     for item in os.listdir(input_folder):
         if item.endswith('.pdf'):
             merger.append(os.path.join(input_folder, item))
             print(f"File {item} merged")
     output_file_path = os.path.join(output_folder, 'merged.pdf')
+    # Write the merger into the resulting pdf
     merger.write(output_file_path)
     merger.close()
--- src/splitter.py
+++ src/splitter.py
@@ -3,15 +3,24 @@ import os
 
 
 def split_pdf(input_pdf, output_folder):
+    """
+    Splits a pdf into multiple pdfs, each containing one page
+    :param input_pdf: The file to be split
+    :param output_folder: The folder that should hold the new pdfs
+    """
+    # Clear the output folder
+    # TODO: CLEAN CODE THE REPEATS IN MERGER.PY
     for file in os.listdir(output_folder):
         file_path = os.path.join(output_folder, file)
         if os.path.isfile(file_path):
             os.remove(file_path)
 
+    # Open the input pdf
     with open(input_pdf, "rb") as pdf_file:
         reader = PyPDF2.PdfReader(pdf_file)
         num_pages = len(reader.pages)
 
+        # Iterate through the input pdf and write each page to a new, separate pdf
         for page_num in range(num_pages):
             writer = PyPDF2.PdfWriter()
             writer.add_page(reader.pages[page_num])
