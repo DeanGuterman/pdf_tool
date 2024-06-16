b72c4909c46f8f7309a2f6f61168c61c7a52042b
DeanGuterman <deangut@gmail.com>
2024-06-16 22:54:07 +0300

    Make the splitter unit test more robust

--- tests/test_splitter.py
+++ tests/test_splitter.py
@@ -9,17 +9,28 @@ from splitter import split_pdf
 
 
 @pytest.fixture
-def setup_files(tmp_path):
-    test_pdf = tmp_path / "test.pdf"
-    writer = PdfWriter()
-    writer.add_blank_page(width=72, height=72)
-    with test_pdf.open("wb") as f:
-        writer.write(f)
-    return test_pdf
+def create_pdf(tmp_path):
+    def _create_pdf(size=(72, 72), num_pages=1):
+        pdf_path = tmp_path / f"test_{size[0]}x{size[1]}_{num_pages}pages.pdf"
+        writer = PdfWriter()
+        for _ in range(num_pages):
+            writer.add_blank_page(width=size[0], height=size[1])
+        with pdf_path.open("wb") as f:
+            writer.write(f)
+        return pdf_path
+    return _create_pdf
 
 
-def test_split(tmp_path, setup_files):
-    output_dir = tmp_path / "_output" # Underscore (_) is so it shows up last in the folder
+@pytest.mark.parametrize("pdf_properties", [
+    {"size": (72, 72), "num_pages": 1},
+    {"size": (72, 72), "num_pages": 2},
+    {"size": (72, 72), "num_pages": 8},
+    {"size": (144, 144), "num_pages": 2},
+    {"size": (144, 144), "num_pages": 4},
+])
+def test_split(tmp_path, pdf_properties, create_pdf):
+    pdf_file = create_pdf(size=pdf_properties["size"], num_pages=pdf_properties["num_pages"])
+    output_dir = tmp_path / "_output"
     output_dir.mkdir()
 
     split_pdf(tmp_path, output_dir)
